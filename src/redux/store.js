3df1190228d4c2f9e407450a3996f5df31a6ca8e
premkgupta <premg9554@gmial.com>
2023-10-13 06:43:49 +0530

    signup file setup

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,9 @@ const store = configureStore({
     reducer: {
         auth: authReducer
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}),
     devTools: true
 });
+
 
 export default store;
